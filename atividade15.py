a9600ae2c02b25bdbc2a86d88284c3572385efe3
EVERTON FERNANDES <evertonsilvaoficiallll@gmail.com>
2025-04-09 21:04:37 -0300

    Add files via upload

---
+++ atividade15.py
@@ -0,0 +1,20 @@
+n = int(input ('Digite um número para calcular a tabuada:'))
+
+
+
+
+for m in range (1,11):
+    print (f'{n}*{m} = {n*m}')
+
+   
+
+
+ciclos = 1 
+
+while ciclos <= 10: 
+    resultado = n * ciclos 
+    print(f'{n} * { ciclos} = {n*ciclos}')
+
+    #incremento 
+
+    ciclos += 1
